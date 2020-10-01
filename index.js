91e1a594a47f7c50249f1b44ab4bc2adb6eaccf6
danilopezyejas <lopezyejas@gmail.com>
2020-10-01 19:56:21 -0300

    Consultar intents

--- index.js
+++ index.js
@@ -39,44 +39,6 @@ app.post('/send-msg',(req,res)=>{
     }); // End of .catch
 })
 
-async function runSample(projectId, msg) {
-
-  // Create a new session
-  const sessionClient = new dialogflow.SessionsClient();
-  const sessionPath = sessionClient.sessionPath(projectId, sessionId);
-
-  // The text query request.
-  const request = {
-    session: sessionPath,
-    queryInput: {
-      text: {
-        // The query to send to the dialogflow agent
-        text: msg,
-        // The language used by the client (en-US)
-        languageCode: "es",
-      },
-    }, // End queryInput:
-  }; // End const request
-
-  // Send request and log result
-  const responses = await sessionClient.detectIntent(request);
-
-  const result = responses[0].queryResult;
-
-  // console.log(result['fulfillmentText']);
-  const fulfillmentTextReply = result.fulfillmentText;
-
-  // if (result.intent) {
-  //   console.log(`  Intent: ${result.intent.displayName}`);
-  // } else {
-  //   console.log(`  No intent matched.`);
-  // }
-
-  return result['fulfillmentText'];
-
-} // End of this function: async function runSample
-
-
 app.listen(port,()=>{
   console.log("Servidor corriendo en el puerto: "+port);
 })
