9efc4355eed2ff028c99d05208630702ff84de87
danilopezyejas <lopezyejas@gmail.com>
2020-10-15 11:21:14 -0300

    Integracion a heroku

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const ChatbotId = "chatbot-pablot-290222";
 
