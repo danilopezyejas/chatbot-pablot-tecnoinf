0dd05b52c6c40e981e4f75c3efb5311b38bbdb12
danilopezyejas <lopezyejas@gmail.com>
2020-10-15 20:34:08 -0300

    Prueba con webhook y heroku

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ const route = require('./routes/route');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({
-  extended: false
-}))
-
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -25,27 +21,3 @@ app.use('/', route);
 app.listen(port,()=>{
   console.log("Servidor corriendo en el puerto: "+port);
 })
-
-// Imports the Google Cloud client library.
-const {Storage} = require('@google-cloud/storage');
-
-// Instantiates a client. If you don't specify credentials when constructing
-// the client, the client library will look for credentials in the
-// environment.
-const storage = new Storage();
-// Makes an authenticated API request.
-async function listBuckets() {
-  try {
-    const results = await storage.getBuckets();
-
-    const [buckets] = results;
-
-    console.log('Buckets:');
-    buckets.forEach((bucket) => {
-      console.log(bucket.name);
-    });
-  } catch (err) {
-    console.error('ERROR:', err);
-  }
-}
-listBuckets();
--- routes/route.js
+++ routes/route.js
@@ -8,9 +8,39 @@ const crear_intent = require('../src/nuevo_intent');
 const ChatbotId = "chatbot-pablot-290222";
 
 router.get('/', (req,res)=>{
-  console.log("ERROR");
-  res.send("ERROR")
-})
+  console.log("ERROR GET");
+  res.send("ERROR GET");
+});
+
+router.post('/', (req,res)=>{
+  // console.log(req.body.queryResult.intent.displayName);
+  let acction = req.body.queryResult.intent.displayName;
+  console.log(acction);
+  switch (acction) {
+    case "suma":
+      let num1 = parseFloat(req.body.queryResult.parameters.num1);
+      let num2 = parseFloat(req.body.queryResult.parameters.num2);
+      let sum = num1 + num2;
+      response = num1 + " + " + num2 + " es " + sum;
+      res.json({
+          "fulfillmentText": response
+      });
+      break;
+    case "Materias primer semestre":
+      if (req.body.queryResult.intent.name.includes("3e6c0259-37f6-47e9-a99e-4499d9c31dbf")
+          && req.body.queryResult.queryText.includes("segundo")) {
+        // res.json({
+        //     "fulfillmentText": "Otro semestre"
+        // });    //Esto tiene que devolver Diego
+        // res.redirect('http://google.com'); //redireccionar a la aplicacion de Diego
+      }
+      break;
+    default:
+      console.log("ERROR POST");
+      res.send("ERROR POST");
+      break;
+  }
+});
 
 router.post('/send-msg', (req,res)=>{
   consultar_intent.buscar_intent(ChatbotId, req.body.MSG)
