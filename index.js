8fef177daa25551049433b82724ddfe58246876d
danilopezyejas <lopezyejas@gmail.com>
2020-10-15 12:38:44 -0300

    Integracion a heroku

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 // const ngrok = require('ngrok');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -25,3 +25,27 @@ app.use('/', route);
 app.listen(port,()=>{
   console.log("Servidor corriendo en el puerto: "+port);
 })
+
+// Imports the Google Cloud client library.
+const {Storage} = require('@google-cloud/storage');
+
+// Instantiates a client. If you don't specify credentials when constructing
+// the client, the client library will look for credentials in the
+// environment.
+const storage = new Storage();
+// Makes an authenticated API request.
+async function listBuckets() {
+  try {
+    const results = await storage.getBuckets();
+
+    const [buckets] = results;
+
+    console.log('Buckets:');
+    buckets.forEach((bucket) => {
+      console.log(bucket.name);
+    });
+  } catch (err) {
+    console.error('ERROR:', err);
+  }
+}
+listBuckets();
