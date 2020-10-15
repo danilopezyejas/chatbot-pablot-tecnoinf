15786d9ab85cf9f3c30b3d5d3dffa8bc08188b70
danilopezyejas <lopezyejas@gmail.com>
2020-10-15 12:23:56 -0300

    Integracion heroku

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 require('dotenv').config();
-// const dialogflow = require("dialogflow");
-// const dialogflow = require("@google-cloud/dialogflow");
-
 // const ngrok = require('ngrok');
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route');
 const app = express();
 const port = process.env.PORT || 5000;
-
-const ChatbotId = "chatbot-pablot-290222";
-
-
-/**
- * Send a query to the dialogflow agent, and return the query result.
- * @param {string} projectId The project to be used
- */
 
 app.use(bodyParser.urlencoded({
   extended: false
