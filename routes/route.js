ba2e5a7d831447520a525ff3001178bd207f3046
danilopezyejas <lopezyejas@gmail.com>
2020-10-26 06:02:09 -0300

    se cambio redirect por fetch

--- routes/route.js
+++ routes/route.js
@@ -9,69 +9,83 @@ const ChatbotId = "chatbot-pablot-290222";
 const ServidorDiego = 'http://localhost:8080/';
 
 let usuarioPregunton = 0;
-let respuesta = ""; 
+let respuesta = "";
+
 router.get('/', (req,res)=>{
   console.log("ERROR GET");
   res.send("ERROR GET");
 });
 router.post('/ultima', (req,res)=>{
+  if(this.respuesta == 'Debe iniciar sesion para responder esta pregunta')
+    res.status(403).send('Debe iniciar sesion para responder esta pregunta');
+  if(this.respuesta == 'A ocurido un error! No se encontro lo solicitado')
+    res.status(404).send('A ocurido un error! No se encontro lo solicitado');
   res.send({Reply: this.respuesta})
 });
 //Atiendo los intent que funcionan con webhook
 router.post('/contexto', (req,res)=>{
+  const body = { id: usuarioPregunton };
   let acction = req.body.queryResult.intent.displayName;
   console.log(acction);
+
   //Solo se respondera este tipo de preguntas a usuarios logeados
   if (usuarioPregunton == "0") {
     this.respuesta = 'Debe iniciar sesion para responder esta pregunta';
-   // res.status(403).send('Debe iniciar sesion para responder esta pregunta');
   }else {
     switch (acction) {
-      case "suma":
-        let num1 = parseFloat(req.body.queryResult.parameters.num1);
-        let num2 = parseFloat(req.body.queryResult.parameters.num2);
-        let sum = num1 + num2;
-        response = num1 + " + " + num2 + " es " + sum;
-        res.json({
-            "fulfillmentText": response
-        });
-        break;
       case "Materias primer semestre":
       //Si el usuario quere saber a que materias se puede anotar de un semestre distindo al primero
       //se calcula que materias tiene aprobadas y se le responde
-        if (!req.body.queryResult.queryText.includes("primero")) {
-          res.redirect(ServidorDiego + 'preguntas/FAQcal6'); //redireccionar a la aplicacion de Diego
+      let ingreso = req.body.queryResult.queryText;
+        if (ingreso != "primero" || ingreso != "Primer" || ingreso != "1") {
+          fetch(ServidorDiego + 'preguntas/FAQcal6',{
+                  method: 'POST',
+                  body: JSON.stringify(body),
+                  headers: { 'Content-Type': 'application/json' }
+          })
+            .then(res => res.json()) // expecting a json response
+            .then(json => this.respuesta = json.Reply );
+          break;
         }
       //En caso de que quiera saber del primer semestre dialogflow se encarga de responder
         break;
       case "Cantidad de creditos":
-        res.redirect(ServidorDiego + 'preguntas/FAQcal1'); //redireccionar a la aplicacion de Diego
+        fetch(ServidorDiego + 'preguntas/FAQcal1',{
+                method: 'POST',
+                body: JSON.stringify(body),
+                headers: { 'Content-Type': 'application/json' }
+        })
+          .then(res => res.json()) // expecting a json response
+          .then(json => this.respuesta = json.Reply );
         break;
       case "Creditos restantes":
-        const body = { id: usuarioPregunton };
-        fetch(ServidorDiego + 'preguntas/FAQcal2',{ method: 'POST', body: JSON.stringify(body),headers: { 'Content-Type': 'application/json' } })
-      .then(res => res.json()) // expecting a json response
-      .then(json => 
-        this.respuesta = json.Reply 
-        );
-
-       // res.redirect(ServidorDiego + 'preguntas/FAQcal2/'+usuarioPregunton); //redireccionar a la aplicacion de Diego
+        fetch(ServidorDiego + 'preguntas/FAQcal2',{
+                method: 'POST',
+                body: JSON.stringify(body),
+                headers: { 'Content-Type': 'application/json' }
+        })
+          .then(res => res.json()) // expecting a json response
+          .then(json => this.respuesta = json.Reply );
         break;
       case "Pasantia":
-        res.redirect(ServidorDiego + 'preguntas/FAQcal3'); //redireccionar a la aplicacion de Diego
+        fetch(ServidorDiego + 'preguntas/FAQcal3',{
+                method: 'POST',
+                body: JSON.stringify(body),
+                headers: { 'Content-Type': 'application/json' }
+        })
+          .then(res => res.json()) // expecting a json response
+          .then(json => this.respuesta = json.Reply );
         break;
       default:
         console.log("ERROR POST");
-        res.status(404).send('A ocurido un error! No se encontro lo solicitado');
+        this.respuesta = 'A ocurido un error! No se encontro lo solicitado';
         break;
     }
   }
 });
 
 router.post('/send-msg', (req,res)=>{
-  
   usuarioPregunton = req.body.id;
-  console.log("pregunta");
   consultar_intent.buscar_intent(ChatbotId, req.body.MSG)
     .then((results) => {
       res.send({Reply: results})
